c15057fb95a8f025b0f87c5cbfd58895c85cbb06
Kimberly Lovering <klovering@expedia.com>
2016-02-04 14:08:15 -0600

    added three initial views

---
+++ AstroObservationLog/AddViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddViewController.swift
+//  AstroObservationLog
+//
+//  Created by Kimberly Lovering on 2/4/16.
+//  Copyright © 2016 Lovering. All rights reserved.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
